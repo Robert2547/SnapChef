5950a51ea246366b12bbc8b786b80f7acfe6f2d6
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2024-06-22 21:24:56 -0400

    Styling

--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -8,7 +8,7 @@ const SideBar = () => {
     <nav class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border">
       <a
         href="#profile"
-        class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50"
+        class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-emerald-600 dark:text-sky-50"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -30,7 +30,7 @@ const SideBar = () => {
 
       <a
         href="#analytics"
-        class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800"
+        class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-green-200 dark:text-gray-400 dark:hover:bg-slate-800"
       >
         <MdFavoriteBorder style={{ fontSize: "1.5rem" }} />
         <small class="text-center text-xs font-medium"> Favorite </small>
